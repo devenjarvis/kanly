385d80a4420933ae39197bcd9624beb8f1d3eba7
devenjarvis <devenjarvis@gmail.com>
2026-05-16 19:57:29 -0400

    [task 6] generate dispatcher source with per-mutant case branches

--- internal/schema/template.go
+++ internal/schema/template.go
@@ -1,12 +1,5 @@
 package schema
 
-import (
-	"bytes"
-	"text/template"
-
-	"github.com/devenjarvis/cauldron/internal/mutation"
-)
-
 const dispatcherSrc = `package {{.PkgName}}
 
 import (
@@ -60,36 +53,3 @@ func __cMutInt(a, b int, op int, mutID int) int {
 	panic("__cMutInt: unknown op")
 }
 `
-
-func mutantExpr(m mutation.Mutation) string {
-	switch m.Mutant {
-	case "+":
-		return "a + b"
-	case "-":
-		return "a - b"
-	case "*":
-		return "a * b"
-	case "/":
-		return "a / b"
-	}
-	return "a + b"
-}
-
-// RenderDispatcher renders the schema dispatcher source for the given package and mutations.
-func RenderDispatcher(pkgName string, muts []mutation.Mutation) (string, error) {
-	tmpl, err := template.New("dispatcher").Funcs(template.FuncMap{
-		"mutantExpr": mutantExpr,
-	}).Parse(dispatcherSrc)
-	if err != nil {
-		return "", err
-	}
-
-	var buf bytes.Buffer
-	if err := tmpl.Execute(&buf, map[string]interface{}{
-		"PkgName": pkgName,
-		"Muts":    muts,
-	}); err != nil {
-		return "", err
-	}
-	return buf.String(), nil
-}
